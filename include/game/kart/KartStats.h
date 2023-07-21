a67fa4c6c3971aa2adbf9fc8bd9595204c4a5454
CLF78 <29682335+CLF78@users.noreply.github.com>
2023-07-21 14:45:29 +0200

    Custom CC: update regular acceleration

---
+++ include/game/kart/KartStats.h
@@ -0,0 +1,63 @@
+#include <kamek.h>
+
+class KartStats {
+public:
+
+    enum WheelCount {
+        WHEELS_4,
+        WHEELS_2,
+        WHEELS_2_QUACKER,
+        WHEELS_3,
+    };
+
+    enum DriftType {
+        OUTSIDE_DRIFT_KART,
+        OUTSIDE_DRIFT_BIKE,
+        INSIDE_DRIFT_BIKE,
+    };
+
+    enum WeightClass {
+        LIGHT,
+        MEDIUM,
+        HEAVY,
+    };
+
+    struct RotatingItemObjParams {
+        float zRadius;
+        float xRadius;
+        float yDist;
+        float zDist;
+    };
+
+    int wheelCount;
+    int vehicleType;
+    int weightClass;
+
+    float _C;
+    float weight;
+    float bumpDeviationLevel;
+    float baseSpeed;
+    float handlingSpeedMultiplier;
+    float tilt;
+    float standard_acceleration_as[4];
+    float standard_acceleration_ts[3];
+    float drift_acceleration_as[2];
+    float drift_acceleration_ts;
+    float manualHandling;
+    float autoHandling;
+    float handlingReactivity;
+    float manualDrift;
+    float automaticDrift;
+    float driftReactivity;
+    float outsideDriftTargetAngle;
+    float outsideDriftDecrement;
+
+    int mtDuration;
+    float speedFactors[32];
+    float handlingFactors[32];
+
+    RotatingItemObjParams rotatingItemObjParams;
+    float vertical_tilt;
+    float megaScale;
+    float axleDistance;
+};
