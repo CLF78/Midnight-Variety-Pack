40630e716f991c31d14fae53f85428b58df56613
CLF78 <29682335+CLF78@users.noreply.github.com>
2024-02-19 17:53:32 +0100

    Add files i forgot to include in the previous commit

---
+++ payload/wiimmfi/ConnectionMatrix.cpp
@@ -0,0 +1,46 @@
+#include <common/Common.hpp>
+#include <dwc/dwc_main.h>
+#include <dwc/dwc_match.h>
+#include <nw4r/ut/Lock.hpp>
+#include <wiimmfi/ConnectionMatrix.hpp>
+#include <wiimmfi/Natneg.hpp>
+#include <wiimmfi/Reporting.hpp>
+
+namespace Wiimmfi {
+namespace ConnectionMatrix {
+
+u32 sRecvConnMtx[12];
+
+void ResetRecv() {
+
+    // Get the node info for the aid, if it's null reset the received matrix
+    for (int aid = 0; aid < 12; aid++) {
+        if (!DWCi_NodeInfoList_GetNodeInfoForAid(aid))
+            sRecvConnMtx[aid] = 0;
+    }
+}
+
+void Update() {
+
+    // Lock interrupts (is this even needed?)
+    nw4r::ut::AutoInterruptLock lock;
+
+    // Failsafe (is this even needed?)
+    if (!stpMatchCnt)
+        return;
+
+    // Compute the connection matrix
+    u32 aidsConnectedToMe = 0;
+    for (int i = 0; i < stpMatchCnt->nodeInfoList.nodeCount; i++) {
+        u32 aid = stpMatchCnt->nodeInfoList.nodeInfos[i].aid;
+        if (DWCi_GetGT2Connection(aid))
+            aidsConnectedToMe |= 1 << i;
+    }
+
+    // Report it to the server and share it with the clients
+    Reporting::ReportConnectionMatrix(aidsConnectedToMe);
+    Natneg::SendConnMtxCommand(aidsConnectedToMe);
+}
+
+} // namespace ConnectionMatrix
+} // namespace Wiimmfi
