66a9d25994e3eaaff1213838670c9ebcaf2c20c5
CLF78 <29682335+CLF78@users.noreply.github.com>
2024-03-05 09:28:10 +0100

    Don't write code at night...

--- payload/game/obj/ObjDossun.cpp
+++ payload/game/obj/ObjDossun.cpp
@@ -1,6 +1,5 @@
 #include <common/Common.hpp>
 #include <game/gfx/AnmHolder.hpp>
-#include <game/gfx/ModelTransformator.hpp>
 
 ////////////////////
 // Game Bug Fixes //
@@ -8,9 +7,8 @@
 
 // Prevent Thwomps from crashing the game when they are hit before they land the first time
 // Credits: MrBean35000vr
-kmBranchDefCpp(0x80760A84, 0x80760A98, float, ModelTransformator* transformator, int idx) {
+kmBranchDefCpp(0x80760A88, 0x80760A98, float, AnmHolder* anm) {
 
 	// Get animation frame count, otherwise a dummy value
-	AnmHolder* anm = transformator->GetAnmHolder(idx);
 	return (anm != nullptr) ? anm->GetFrameCount() : 0.0f;
 }
