ab491e209fe6e476d4b1bf1c1c45db3c43169c30
CLF78 <29682335+CLF78@users.noreply.github.com>
2024-03-12 11:23:43 +0100

    Save Expansion: Skip region check when loading save

--- payload/game/system/SaveManager.cpp
+++ payload/game/system/SaveManager.cpp
@@ -13,9 +13,13 @@ kmCallDefCpp(0x80543DDC, u32) {
 }
 
 // SaveManager::createInstance() patch
-// Construct the expansion data
+// Construct the expansion data and initialize it
 kmBranchDefCpp(0x80543ED8, NULL, SaveManager*, SaveManager* self) {
     if (self)
         SaveExpansion::construct(&self->expansion);
     return self;
 }
+
+// SaveManager::init() patch
+// Allow saves to be region-free
+kmWrite32(0x80544930, 0x60000000);
