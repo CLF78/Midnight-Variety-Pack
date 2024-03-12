4a4bb07e99b4ccb53b62c63d45ebb4eed7a16e4a
CLF78 <29682335+CLF78@users.noreply.github.com>
2024-03-12 17:57:54 +0100

    TTs: Disable Nintendo staff ghosts

--- payload/game/ui/page/GhostManagerPage.cpp
+++ payload/game/ui/page/GhostManagerPage.cpp
@@ -11,3 +11,11 @@ kmWrite32(0x805E1EB0, 0x60000000);
 // GhostManagerPage::setupGhostRace() patch
 // Prevent overwriting CC when starting a time trial against a ghost
 kmWrite32(0x805E1D0C, 0x60000000);
+
+////////////////////////
+// Time Trial Support //
+////////////////////////
+
+// GhostManagerPage::onInit() patch
+// Disable Nintendo staff ghost loading
+kmBranch(0x805E0F1C, 0x805E0F50);
