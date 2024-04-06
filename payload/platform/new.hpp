7ac463e48eac20320da9c2e34d9d5e7913d34241
CLF78 <29682335+CLF78@users.noreply.github.com>
2024-04-06 11:47:23 +0200

    Refactor: Implement placement new properly

--- payload/platform/new.hpp
+++ payload/platform/new.hpp
@@ -1,10 +1,11 @@
 #include <common/Common.hpp>
 #include <egg/core/eggHeap.hpp>
 
-void* operator new(size_t size, void* buffer);
 void* operator new(size_t size, int align);
 void* operator new(size_t size, EGG::Heap* heap, int align);
-
-void* operator new[](size_t size, void* buffer);
 void* operator new[](size_t size, int align);
 void* operator new[](size_t size, EGG::Heap* heap, int align);
+
+// Implement placement new
+inline void* operator new(size_t size, void* buffer) { return buffer; }
+inline void* operator new[](size_t size, void* buffer) { return buffer; }
