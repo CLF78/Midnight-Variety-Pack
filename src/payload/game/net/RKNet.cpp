30b462a0c866a1ce86afe99a809b34fd217cdaad
CLF78 <29682335+CLF78@users.noreply.github.com>
2024-06-17 10:16:05 +0200

    Network: Fix null pointer crash

--- src/payload/game/net/RKNet.cpp
+++ src/payload/game/net/RKNet.cpp
@@ -22,8 +22,9 @@ REPLACE void UserRecvCallback(u32 aid, void* data, u32 dataLength) {
         return;
 
     // Get the PID of the AID that sent the packet
-    DWCNodeInfo* info = DWCi_NodeInfoList_GetNodeInfoForAid(aid);
-    u32 pid = info->profileId;
+    u32 pid = -1;
+    if (DWCNodeInfo* info = DWCi_NodeInfoList_GetNodeInfoForAid(aid))
+        u32 pid = info->profileId;
 
     // Verify the checksum
     // The game already does this later, but we shouldn't disconnect a player because a packet got corrupted
