7b9fabacd17fb1d8b964a43caa78d86cd27e922a
CLF78 <29682335+CLF78@users.noreply.github.com>
2024-08-01 23:18:39 +0200

    Refactor: Ensure macros are properly escaped

--- src/payload/platform/stdarg.h
+++ src/payload/platform/stdarg.h
@@ -12,7 +12,7 @@ typedef struct {
 	char* reg_save_area;
 } va_list;
 
-#define va_start(ap,fmt) ((void)fmt,__builtin_va_info(&ap))
+#define va_start(ap,fmt) ((void)(fmt),__builtin_va_info(&(ap)))
 #define va_end(ap) ((void)0)
 
 #ifdef __cplusplus
