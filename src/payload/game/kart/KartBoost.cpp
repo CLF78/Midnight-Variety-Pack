0c67e030ffce2b3e8c96b8f22a8344dc6a2c5279
CLF78 <29682335+CLF78@users.noreply.github.com>
2024-08-16 11:31:25 +0200

    Ultra Mini-Turbo: Fix acceleration bug caused by missing braces

--- src/payload/game/kart/KartBoost.cpp
+++ src/payload/game/kart/KartBoost.cpp
@@ -13,7 +13,8 @@ kmListHookDefCpp(RaceStartHook) {
     KartBoost::boostAccelerations[KartBoost::UMT] = 3.3f;
 
     const u32 engineClass = RaceConfig::instance->raceScenario.settings.engineClass;
-    if (engineClass > RaceConfig::Settings::CC_150)
+    if (engineClass > RaceConfig::Settings::CC_150) {
         KartBoost::boostAccelerations[KartBoost::MT_OR_START] *= KartMove::speedModifiers[engineClass];
         KartBoost::boostAccelerations[KartBoost::UMT] *= KartMove::speedModifiers[engineClass];
+    }
 }
